efe36871925b16039ddbd12458f81650fe4adbdd
Danilo Alves <danilo.alves@luizalabs.com>
2019-01-03 10:31:33 -0200

    Validação de dados

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     app.get('/formulario_inclusao_noticia', function (req, res) {
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao : {}, noticia : {} });
     });
 
 
@@ -11,8 +11,16 @@ module.exports = function (app) {
         req.assert('resumo','Resumo é obrigatório').notEmpty();
         req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
         req.assert('autor','Autor é obrigatório').notEmpty();
-        req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        //req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
         req.assert('noticia','Notícia é obrigatório').notEmpty();
+
+      var errors = req.validationErrors();
+
+        if(errors)
+        {
+          res.render('admin/form_add_noticia', {validacao : errors, noticia : noticia});
+          return;
+        }
 
         var connection = app.config.dbConnection();
         var noticiasModel = new app.app.models.NoticiasDAO(connection);
